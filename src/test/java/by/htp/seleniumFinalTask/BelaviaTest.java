a171bef73ed7ab0f1309d24681ba7f3e7fc8e5b5
BelkoNatalia <khankevich@inbox.ru>
2018-08-04 09:59:24 +0300

    add simple Steps and AbstractPage and DriverSingleton and BelaviaTest

---
+++ src/test/java/by/htp/seleniumFinalTask/BelaviaTest.java
@@ -0,0 +1,29 @@
+package by.htp.seleniumFinalTask;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import by.htp.seleniumFinalTask.steps.Steps;
+
+public class BelaviaTest {
+	private Steps steps;
+
+	@BeforeMethod(description = "Init browser")
+	public void setUp() {
+		steps = new Steps();
+		steps.initBrowser();
+	}
+
+	@Test(description = "One side")
+	public void getListTickersOneSide() {
+		System.out.println("Run step");;
+		
+	}
+
+	@AfterMethod(description = "Stop Browser")
+	public void stopBrowser() {
+		steps.closeDriver();
+	}
+
+}
---
+++ src/test/java/by/htp/seleniumFinalTask/steps/Steps.java
@@ -0,0 +1,22 @@
+package by.htp.seleniumFinalTask.steps;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+import by.htp.seleniumFinalTask.driver.DriverSingleton;
+
+public class Steps {
+	private WebDriver driver;
+
+	private final Logger logger = LogManager.getRootLogger();
+
+	public void initBrowser() {
+		driver = DriverSingleton.getDriver();
+	}
+
+	public void closeDriver() {
+		DriverSingleton.closeDriver();
+	}
+	
+}
